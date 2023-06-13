da0ab68d0dcfa0630e70fe95554ec3f951014576
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-13 16:39:50 +0200

    setting up email generation software (not working)

--- js/script.js
+++ js/script.js
@@ -3,11 +3,23 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-
+            newEmail: "",
         }
     },
 
     methods: {
+        getRandomEmail(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) =>{
+                const result = response.data;
 
+                this.newEmail = result;
+
+                console.log(this.newEmail);
+            })
+        }
+    },
+
+    created(){
+        this.getRandomEmail();
     }
 }).mount("#app");
